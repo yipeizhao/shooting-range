b992b31f5b6bc1e33a96d0949b4d99658d251415
Yipei Zhao <zhaoyipei99@gmail.com>
2022-05-14 23:31:49 +0100

    mostly done

--- Gamev2.py
+++ Gamev2.py
@@ -1,3 +1,4 @@
+import random
 class Game:
     def __init__(self):
         self.round_no = 0
@@ -7,14 +8,14 @@ class Game:
         self.TARGET_MAX = 10 +4 
         self.RESPAWN_PROB = 0.5
         self.score = 0
-        self.player = self.Player([3,self.width-1])
+        self.player = self.Player(3,self.width - 1)
         if self.width%2 != 0:
-            self.TARGET_LOCATION = list(range(0,self.width,2))
-            self.target = [((item,0),9) for item in list(range(0,self.width,2))]
+            self.TARGET_LOCATION = set([(0,item) for item in list(range(0,self.width,2))])
+            self.target = [((0,item),9) for item in list(range(0,self.width,2))]
             self.target = [self.Target(item[0],item[1]) for item in self.target]
         else:
-            self.TARGET_LOCATION = list(range(0,self.width-1,2))
-            self.target = [((item,0),9) for item in list(range(0,self.width-1,2))]
+            self.TARGET_LOCATION = [(0,item) for item in list(range(0,self.width,2))]
+            self.target = [((0,item),9) for item in list(range(0,self.width-1,2))]
             self.target = [self.Target(item[0],item[1]) for item in self.target]
     
     def create_board(self,width):
@@ -51,20 +52,92 @@ class Game:
         print("Score: "+str(self.score))
         print("")
     
+    # =============================================================================
+    # Taking the command and move the player
+    # type: str
+    # rtype: None    
+    # =============================================================================
+    def movement(self,command):
+        if command == "SOUTH":
+            self.player.row +=1
+        
+        elif command == "NORTH":
+            # Only operates if player is in row 3
+            if self.player.row == 3:
+                self.player.row -= 1
+            else:
+                raise Exception("You are out of the box")
+                
+        elif command == "WEST":
+            self.player.col-= 1
+            
+        elif command == "EAST":
+            self.player.col += 1
+            
+        elif command == "PASS":
+            pass
+            
+        elif command == "SHOOT":
+            # Detect wether a the player is in row 2 and there is a target infront
+            if self.player.row == 2:
+                for item in self.target:
+                        if item.col == self.player.col:
+                            self.target.remove(item)
+                            self.score += 1
+        else:
+            raise Exception("Invalid movement")
+            # Restraints:
+            # 1. Player loc column no +1 < width
+            # 2. Player loc column is > 0(not going left out of the map)
+            # 3. Player loc row is not >3(not going below the map)
+            # 4. Player loc row = 2 and can only go in even columns
+        if self.player.col+1 > self.width or \
+           self.player.col<0 or \
+           self.player.row>3 or \
+           bool(self.player.col%2!=0 and self.player.row==2):
+            raise Exception("You are out of the box")
+    
+    def target_update(self):
+        new_targets = []
+        for item in self.target:
+            if item.remaining_round != 1:
+                new_targets.append(item)
+        for item1 in (self.TARGET_LOCATION - set([item.loc for item in self.target])):
+            if random.random()<self.RESPAWN_PROB:
+                new_targets.append(self.Target(item1,9))
+        self.target = new_targets
+        
+    
     class Target(object):
         def __init__(self,loc,remaining_round):
             self.loc = loc
             self.row = self.loc[0]
             self.col = self.loc[1]
             self.remaining_round = remaining_round
+        def update(self):
+            self.remaining_round -= 1
             
         
     class Player(object):
-        def __init__(self,init_loc):
-            self.init_loc = init_loc
-            self.row = init_loc[0]
-            self.col = init_loc[1]
+        def __init__(self,init_row,init_col):
+            self.row = init_row
+            self.col = init_col
+                        
         
+    # =============================================================================
+    # An interactive way to play the game, taking the command from the user
+    # =============================================================================
+    def interactive(self):
+         while(self.round_no < self.ROUND_MAX and self.target_no < self.TARGET_MAX):
+            print("Round no: " + str(self.round_no))
+            self.display()
+            command = input("Please state your next move: ")
+            self.round_no += 1
+            self.movement(command)
+            self.target_update()
+            for item in self.target:
+                item.update()
+            print("Target respawned: " + str(self.target_no))
 # =============================================================================
 # Replace the char c in a string s given index i
 # type s, string
@@ -79,4 +152,4 @@ def string_replacement(s,i,c):
     return new_s
 
 game = Game()
-game.display()
+game.interactive()

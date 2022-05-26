bdc0bbbbcf4e19b094ab9e6815d5976f1568fde6
Yipei Zhao <zhaoyipei99@gmail.com>
2022-05-26 15:17:42 +0100

    improved the readability of the code

--- Gamev2.py
+++ Gamev2.py
@@ -36,7 +36,7 @@ class Game:
         self.RESPAWN_PROB = respawn_prob
         self.score = 0
         self.player = self.Player(0, self.WIDTH - 1)
-        self.TARGET_LOCATION = set([(3, item) for item in list(range(0, self.WIDTH, 2))])
+        self.TARGET_LOCATION = set([tuple((3, item)) for item in list(range(0, self.WIDTH, 2))])
 
         # Initiate targets
         # If no targets are generated initially, randomly choose a target location to spawn a target
@@ -44,11 +44,11 @@ class Game:
         self.target = []
         for i in range(len(self.TARGET_LOCATION)):
             if random.random() < self.RESPAWN_PROB and self.target_no < self.TARGET_MAX:
-                self.target.append(self.Target(list(self.TARGET_LOCATION)[i], 9))
+                self.target.append(self.Target(tuple(self.TARGET_LOCATION)[i], 9))
                 self.target_no += 1
         if len(self.target) == 0:
             init_target = random.choice(list(self.TARGET_LOCATION))
-            self.target = [(self.Target(init_target, 9))]
+            self.target.append(self.Target(init_target, 9))
             self.target_no += 1
 
     # =============================================================================
@@ -65,7 +65,9 @@ class Game:
             self.col = self.loc[1]
             self.remaining_round = remaining_round
 
-        # update is called for every target every round, increment round number by -1
+        # =============================================================================
+        # Update is called for every target every round, increment round number by -1
+        # =============================================================================
         def update(self):
             self.remaining_round -= 1
 
@@ -113,6 +115,7 @@ class Game:
             rows = self.create_board()
         print("Round no: " + str(self.round_no))
         print("Target respawned: " + str(self.target_no))
+
         # Modifying row 1 according to targets
         for item in self.target:
             rows[1] = string_replacement(rows[1], item.col * 2 + 2, item.remaining_round)
@@ -120,7 +123,7 @@ class Game:
         # Modifying row 4 according to player
         if self.player.row == 0:
             rows[4] = string_replacement(rows[4], self.player.col * 2 + 2, "X")
-        else:
+        elif self.player.row == 1:
             rows[3] = string_replacement(rows[3], self.player.col * 2 + 2, "X")
         for item in rows:
             print(item)
@@ -136,7 +139,8 @@ class Game:
     def movement(self, command):
         # Whenever a move is performed, add 1 to round no
         self.round_no += 1
-        # A new variable to store the new location of the player, it is used to check the validity of the new player location
+        # A new variable to store the new location of the player
+        # it is used to check the validity of the new player location
         new_loc = [self.player.row, self.player.col]
         # If SHOOT is being called, we have to determine the validity of the shot
         # has to satisfy: there is a target in the same col and the player has to be 2 units away from a target
@@ -178,11 +182,13 @@ class Game:
         # Else, assign the current player loc to the new loc
 
         # Border between booths
-        # West and east border
+        # West border
+        # East border
         # South border
         # North border
         if (new_loc[0] == 1 and new_loc[1] % 2 != 0) or \
-                (new_loc[1] < 0) or (new_loc[1] > self.WIDTH - 1) or \
+                (new_loc[1] < 0) or \
+                (new_loc[1] > self.WIDTH - 1) or \
                 (new_loc[0] < 0) or \
                 (new_loc[0] > 1):
             # print("You made an invalid move.")
@@ -191,9 +197,7 @@ class Game:
         else:
             self.player.row = new_loc[0]
             self.player.col = new_loc[1]
-        # After player's move, targets are updated
-        for item in self.target:
-            item.update()
+        # Updating targets, including round number reducing and generating new targets
         self.target_update()
         # Determines the termination of the game
         if self.round_no >= self.ROUND_MAX:
@@ -208,20 +212,24 @@ class Game:
     # rtype: None
     # =============================================================================
     def target_update(self):
+        # Terminates the game TARGET_MAX is reached and no more current targets
+        if self.target_no >= self.TARGET_MAX and len(self.target) == 0:
+            self.terminate = True
+
+        # Appending all current targets
         new_targets = []
         for item in self.target:
+            item.update()
             if item.remaining_round > 0:
                 new_targets.append(item)
 
+        # Use all potential targets to minus all current targets to find all empty targets
         for item1 in (self.TARGET_LOCATION - set([item.loc for item in self.target])):
             if random.random() < self.RESPAWN_PROB and self.target_no < self.TARGET_MAX:
                 new_targets.append(self.Target(item1, 9))
                 self.target_no += 1
 
         self.target = new_targets
-        # Determines the termination of the game
-        if self.target_no >= self.TARGET_MAX and len(new_targets) == 0:
-            self.terminate = True
 
     # =============================================================================
     # An interactive way to play the game, taking the command from the user
